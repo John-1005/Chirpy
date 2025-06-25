7a413980ec163889a4e65524a86a32a2f608fbc0
John-1005 <jtgarza48@gmail.com>
2025-06-25 16:03:34 -0700

    Added JWT creation and validation

--- internal/auth/hash.go
+++ internal/auth/hash.go
@@ -1,29 +1,71 @@
-package main
+package auth
 
 import (
+	"fmt"
+	"time"
 
-    "golang.org/x/crypto/bcrypt"
-
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
-
+type CustomClaims struct {
+	UserID    uuid.UUID     `json:"user_id"`
+	ExpiresIn time.Duration `json:"expriresIn"`
+	jwt.RegisteredClaims
+}
 
 func HashPassword(password string) (string, error) {
 
-  hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-  if err != nil {
-    return "", err
-  }
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
 
-  return string(hashedPassword), nil 
+	return string(hashedPassword), nil
 }
 
-
 func CheckPasswordHash(password, hash string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
-  err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-  if err != nil{
-    return err
-  } 
-  return nil
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	claims := CustomClaims{
+		UserID:    userID,
+		ExpiresIn: expiresIn,
+		RegisteredClaims: jwt.RegisteredClaims{
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+			Issuer:    "Chirpy",
+			Subject:   fmt.Sprintf("%d", userID),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString(tokenSecret)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+
+	token, err := jwt.ParseWithClaims(tokenString, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.Nil, err
+	}
+	claims, ok := token.Claims.(*CustomClaims)
+	if !ok || token.Valid {
+		return uuid.Nil, fmt.Errorf("invalid token")
+	}
+
+	return claims.UserID, nil
 }
